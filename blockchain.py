49e3d451456a8b4ffcc389ac1516d1a415bd4250
Enyi10 <o.enyi.10@student.scu.edu.au>
2025-08-17 19:09:30 +0500

    Initial blockchain implementation

---
+++ blockchain.py
@@ -0,0 +1,54 @@
+{
+  "nbformat": 4,
+  "nbformat_minor": 0,
+  "metadata": {
+    "colab": {
+      "provenance": [],
+      "authorship_tag": "ABX9TyMLSlxc4nYmTmJIHYFwM3nN",
+      "include_colab_link": true
+    },
+    "kernelspec": {
+      "name": "python3",
+      "display_name": "Python 3"
+    },
+    "language_info": {
+      "name": "python"
+    }
+  },
+  "cells": [
+    {
+      "cell_type": "markdown",
+      "metadata": {
+        "id": "view-in-github",
+        "colab_type": "text"
+      },
+      "source": [
+        "<a href=\"https://colab.research.google.com/github/Enyi10/simple-blockchain/blob/main/blockchain.py\" target=\"_parent\"><img src=\"https://colab.research.google.com/assets/colab-badge.svg\" alt=\"Open In Colab\"/></a>"
+      ]
+    },
+    {
+      "cell_type": "code",
+      "source": [
+        "# Test the environment\n",
+        "print(\"Hello Blockchain - Project Initialized \")\n"
+      ],
+      "metadata": {
+        "colab": {
+          "base_uri": "https://localhost:8080/"
+        },
+        "id": "_O59rgHDXy9E",
+        "outputId": "f5bcb552-3c6a-406c-bfd8-87762c0042ef"
+      },
+      "execution_count": 1,
+      "outputs": [
+        {
+          "output_type": "stream",
+          "name": "stdout",
+          "text": [
+            "Hello Blockchain - Project Initialized \n"
+          ]
+        }
+      ]
+    }
+  ]
+}
